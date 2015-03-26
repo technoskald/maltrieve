4c7d6bd61e35e782c7deae5168b961659f732521
Kyle Maxwell <kylem@xwell.org>
2015-03-25 22:15:27 -0500

    Add more tests and factor out source list processing

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import requests
 
 
 def test_basic_args():
-    args = maltrieve.setup_args(['-l', 'testlog', '-p', '127.0.0.1:8080', '-d', '/opt/'])
-    assert args.logfile == 'testlog'
+    args = maltrieve.setup_args(['-l', 'maltrieve-test.log', '-p', '127.0.0.1:8080', '-d', '/opt/'])
+    assert args.logfile == 'maltrieve-test.log'
     assert args.proxy == '127.0.0.1:8080'
     assert args.dumpdir == '/opt/'
 
@@ -35,6 +35,25 @@ def test_parse_xml_desc():
         set(['http://example.org/mylist', 'http://example.com/yourlist'])
 
 
+def test_read_cfg():
+    args = maltrieve.setup_args([])
+    cfg = maltrieve.config(args, filename='maltrieve-test.cfg')
+    assert cfg.logfile == 'maltrieve-test.log'
+    assert cfg.dumpdir == '/tmp'
+    assert cfg.black_list is None
+    assert cfg.white_list is None
+    assert cfg.useragent == "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)"
+
+
+def test_override_cfg():
+    args = maltrieve.setup_args(['-d', 'test-archive', '-l', 'another-test.log',
+                                 '-p', '127.0.0.1:8080'])
+    cfg = maltrieve.config(args, filename='maltrieve-test.cfg')
+    assert cfg.proxy == {'http': '127.0.0.1:8080'}
+    assert cfg.dumpdir == 'test-archive'
+    assert cfg.logfile == 'another-test.log'
+
+
 def test_load_hashes(hashfile='test-load-hashes.json'):
     assert maltrieve.load_hashes(hashfile) == \
         set(['d41d8cd98f00b204e9800998ecf8427e'])
@@ -55,3 +74,10 @@ def test_save_urls():
     urls = set(['http://example.com/badurl'])
     maltrieve.save_urls(urls, 'test-save-urls.json')
     test_load_urls('test-save-urls.json')
+
+
+def test_get_sources():
+    args = maltrieve.setup_args([])
+    cfg = maltrieve.config(args, filename='maltrieve-test.cfg')
+    source_lists = maltrieve.process_source_lists(cfg)
+    assert len(source_lists) == len(cfg.source_urls)
